78cbbbe2c159995794098fdba819f6e76e374e55
ryanyogan <ryanyogan@gmail.com>
2020-01-21 08:16:58 -0800

    lil seed file fo' ya

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,15 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Chitter.Repo.insert!(%Chitter.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Chitter.Auth.User
+alias Chitter.Chat.{Conversation, ConversationMember}
+
+alias Chitter.{Auth, Chat}
+
+{:ok, %User{id: u1_id}} = Auth.create_user(%{nickname: "User One"})
+{:ok, %User{id: u2_id}} = Auth.create_user(%{nickname: "User Two"})
+
+{:ok, %Conversation{id: conv_id}} = Chat.create_conversation(%{title: "Ravens are for the birds"})
+
+{:ok, %ConversationMember{}} =
+  Chat.create_conversation_member(%{conversation_id: conv_id, user_id: u1_id, owner: true})
+
+{:ok, %ConversationMember{}} =
+  Chat.create_conversation_member(%{conversation_id: conv_id, user_id: u2_id, owner: false})
