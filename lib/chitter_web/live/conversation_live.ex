d8fed536f5b28ce491ff15595588886f3e16ce5c
ryanyogan <ryanyogan@gmail.com>
2020-01-21 13:45:16 -0800

    pub/sub and what

---
+++ lib/chitter_web/live/conversation_live.ex
@@ -0,0 +1,93 @@
+defmodule ChitterWeb.ConversationLive do
+  require Logger
+
+  use Phoenix.LiveView
+  use Phoenix.HTML
+
+  alias Chitter.{Auth, Chat, Repo}
+
+  def render(assigns) do
+    ~L"""
+    <div>
+      <b>User name:</b> <%= @user.nickname %>
+    </div>
+    <div>
+      <b>Conversation title:</b> <%= @conversation.title %>
+    </div>
+    <div>
+      <%= f = form_for :message, "#", phx_submit: "send_message" %>
+        <%= label f, :content %>
+        <%= text_input f, :content %>
+        <%= submit "Send" %>
+      </form>
+    </div>
+    <div>
+      <b>Messages</b>
+      <%= for message <- @messages do %>
+        <div>
+          <b><%= message.user.nickname %></b>: <%= message.content %>
+        </div>
+      <% end %>
+    </div>
+    """
+  end
+
+  def mount(_assigns, socket) do
+    {:ok, socket}
+  end
+
+  def handle_params(%{"conversation_id" => conversation_id, "user_id" => user_id}, _uri, socket) do
+    ChitterWeb.Endpoint.subscribe("conversation_#{conversation_id}")
+
+    {:noreply,
+     socket
+     |> assign(:user_id, user_id)
+     |> assign(:conversation_id, conversation_id)
+     |> assign_records()}
+  end
+
+  def handle_event(
+        "send_message",
+        %{"message" => %{"content" => content}},
+        %{assigns: %{conversation_id: conversation_id, user_id: user_id, user: user}} = socket
+      ) do
+    case Chat.create_message(%{
+           conversation_id: conversation_id,
+           user_id: user_id,
+           content: content
+         }) do
+      {:ok, new_message} ->
+        new_message = %{new_message | user: user}
+
+        ChitterWeb.Endpoint.broadcast!(
+          "conversation_#{conversation_id}",
+          "new_message",
+          new_message
+        )
+
+      {:error, err} ->
+        Logger.error(inspect(err))
+    end
+
+    {:noreply, socket}
+  end
+
+  def handle_info(%{event: "new_message", payload: new_message}, socket) do
+    updated_messages = socket.assigns[:messages] ++ [new_message]
+
+    {:noreply, socket |> assign(:messages, updated_messages)}
+  end
+
+  defp assign_records(%{assigns: %{user_id: user_id, conversation_id: conv_id}} = socket) do
+    user = Auth.get_user!(user_id)
+
+    conversation =
+      Chat.get_conversation!(conv_id)
+      |> Repo.preload(messages: [:user], conversation_members: [:user])
+
+    socket
+    |> assign(:user, user)
+    |> assign(:conversation, conversation)
+    |> assign(:messages, conversation.messages)
+  end
+end
--- lib/chitter_web/router.ex
+++ lib/chitter_web/router.ex
@@ -18,10 +18,8 @@ defmodule ChitterWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
-  end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", ChitterWeb do
-  #   pipe_through :api
-  # end
+    live "/conversations/:conversation_id/users/:user_id",
+         ConversationLive
+  end
 end
